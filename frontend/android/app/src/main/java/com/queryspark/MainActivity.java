752bfffb3406891a8912532ca27f8cef926bd51f
Keshav Garg <keshav2003garg@gmail.com>
2023-11-16 23:29:05 +0530

    core - [splash screen setup]

--- frontend/android/app/src/main/java/com/queryspark/MainActivity.java
+++ frontend/android/app/src/main/java/com/queryspark/MainActivity.java
@@ -1,12 +1,26 @@
 package com.queryspark;
 
+import android.os.Bundle;  // Splash Screen
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;  // Splash Screen
 
 public class MainActivity extends ReactActivity {
 
+  // Splash Screen
+
+  /*
+    **Uncomment this block to add splash screen**
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+  */
+
+  // Splash Screen
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
