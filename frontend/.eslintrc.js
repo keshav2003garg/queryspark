5cfc2f58a86411bdb2e631e7da107aee2eefb57b
Keshav Garg <keshav2003garg@gmail.com>
2023-11-16 10:31:37 +0530

    chore - [prettier-eslint configs & clear folder structure]

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
 };
