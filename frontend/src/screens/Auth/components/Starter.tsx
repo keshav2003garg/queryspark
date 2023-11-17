71a111b0e991a81879d26fbf2195f837dc097111
Keshav Garg <keshav2003garg@gmail.com>
2023-11-17 20:47:11 +0530

    feat - [auth screen ui]

--- frontend/src/screens/Auth/components/Starter.tsx
+++ frontend/src/screens/Auth/components/Starter.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View, Text, Image, ImageBackground } from 'react-native';
+import {
+    View,
+    Text,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+} from 'react-native';
 
 const Starter: React.FC = () => {
     return (
         <ImageBackground
             className='w-full h-full flex-1'
             source={require('../../../assets/img/starter.png')}>
-            <View className='flex-3 bg-transparent'></View>
-            <View className='flex-1 bg-transparent rounded-[25px]'></View>
+            <View className='flex-4 bg-transparent'></View>
+            <View className='flex-2 mt-14 bg-transparent rounded-[25px]'>
+                <View className='mx-8'>
+                    <Text className='text-4xl text-[#f79a11] font-[Poppins-Medium]'>
+                        Sign in
+                    </Text>
+                    <Text className='mt-4 text-sm text-white font-[Poppins-Light]'>
+                        Sign in with any of your Google or GitHub Account
+                    </Text>
+                    <TouchableOpacity>
+                        <View className='mt-7 p-2.5 bg-white rounded-[50px] flex-row justify-center items-center'>
+                            <Image
+                                className='w-[30px] h-[30px] mx-[10px]'
+                                source={require('../../../assets/icons/google.png')}></Image>
+                            <Text className='text-black text-base font-[Poppins-Medium]'>
+                                Continue With Google
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View className='mt-8 p-2.5 bg-white rounded-[50px] flex-row justify-center items-center'>
+                            <Image
+                                className='w-[30px] h-[30px] mx-[10px]'
+                                source={require('../../../assets/icons/github.png')}></Image>
+                            <Text className='text-black text-base font-[Poppins-Medium]'>
+                                Continue With GitHub
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </ImageBackground>
     );
 };
