a5cee273b6ba23f5fedc543384a876aede8348b6
Keshav Garg <keshav2003garg@gmail.com>
2023-12-25 01:40:30 +0530

    feat - [firebase auth added]

--- frontend/src/actions/auth.action.ts
+++ frontend/src/actions/auth.action.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from '@reduxjs/toolkit';
 
+import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { authorize } from 'react-native-app-auth';
 import Config from 'react-native-config';
@@ -12,7 +13,11 @@ export const googleSignIn = () => async (dispatch: Dispatch) => {
             offlineAccess: true,
         });
         const userInfo = await GoogleSignin.signIn();
-        console.log(userInfo);
+        const googleCredential = auth.GoogleAuthProvider.credential(
+            userInfo.idToken,
+        );
+        const user = await auth().signInWithCredential(googleCredential);
+        console.log(user);
     } catch (error) {
         console.log(error);
     }
@@ -30,12 +35,16 @@ export const githubSignIn = () => async (dispatch: Dispatch) => {
                 authorizationEndpoint:
                     'https://github.com/login/oauth/authorize',
                 tokenEndpoint: 'https://github.com/login/oauth/access_token',
-                revocationEndpoint:
-                    `https://github.com/settings/connections/applications/${Config.GITHUB_CLIENT_ID}`,
+                revocationEndpoint: `https://github.com/settings/connections/applications/${Config.GITHUB_CLIENT_ID}`,
             },
         };
-        const result = await authorize(config);
-        console.log(result);
+        const authState = await authorize(config);
+        const credential = auth.OIDCAuthProvider.credential(
+            'queryspark',
+            authState.accessToken,
+        );
+        const user = await auth().signInWithCredential(credential);
+        console.log(user);
     } catch (error) {
         console.log(error);
     }
