02159342125c483cc9f695d87763c0f7c2d72b55
Keshav Garg <keshav2003garg@gmail.com>
2024-01-09 03:11:01 +0530

    ui/fix - [improved ui when pdf is uploaded]

--- frontend/src/screens/Main/screens/New/screens/Upload/templates/Uploaded.tsx
+++ frontend/src/screens/Main/screens/New/screens/Upload/templates/Uploaded.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 const Uploaded: React.FC = () => {
     return (
         <View className='flex-1'>
             <LottieView
-                style={{ flex: 6 }}
+                style={{ flex: 2 }}
                 source={require('assets/animations/uploaded.json')}
                 autoPlay={true}
                 loop={true}
             />
+            <Text className='text-center text-white text-2xl font-[Poppins-Medium]'>
+                Wait while your chat is being created...
+            </Text>
             <View className='flex-1'></View>
         </View>
     );
